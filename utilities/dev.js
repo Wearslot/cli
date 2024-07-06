c4759b69658394a40633396f1e6b828f1162844c
Akinpelumi Akinniyi <aakinniyi@binghr.io>
2024-07-06 17:06:46 +0100

    updating taojaa storefront

--- utilities/dev.js
+++ utilities/dev.js
@@ -9,20 +9,39 @@ const startDevServer = (ctx, credentials) => {
 
     process.env = {
         ...process.env,
+
+        /**
+         * Adding app PORT environtment
+        */
+        PORT: ctx.port,
+
+        /**
+         * Declearing app root dir in envrioment
+        */
         THEME_DIR: `${ctx.dir}/`,
+
+        /**
+         * Adding storefront api base url environtment
+        */
         TAOJAA_STOREFRONT_API: 'https://storefront-service-prod.taojaa.com/api/v1',
+
+        /**
+         * Adding authentication credentials secret to enviroment 
+        */
         AUTH_SECRET_KEY: credentials.SECRET_KEY,
+
+        /**
+         * Adding store domain name environtment
+        */
         STORE_DOMAIN: `${ctx.theme.store}.taojaa.shop`
-    }
+    };
 
-    const app = require('taojaa-storefront');
+    console.log(`
+    ${chalk.green.bold(`Access on local http://127.0.0.1:${ctx.port}`)}\n
+    To preview on your development store:\n
+    ${chalk.blue.bold('taojaa')} theme push --store <store-name>\n
+    ${chalk.green.bold(`Access your develoment store https://${ctx.theme.store}.taojaa.shop`)}
+    `);
 
-    app.listen(ctx.port, () => {
-        console.log(`
-            ${chalk.green.bold(`Access on local http://127.0.0.1:${ctx.port}`)}\n
-            To preview on your development store:\n
-            ${chalk.blue.bold('taojaa')} theme push --store <store-name>\n
-            ${chalk.green.bold(`Access your develoment store https://${ctx.theme.store}.taojaa.shop`)}
-        `);
-    })
+    require('taojaa-storefront');
 }
